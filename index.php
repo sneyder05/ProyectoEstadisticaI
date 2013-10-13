0cdac12e734d7fa944226f1f5f33d2a481892b58
sneyder <sneyder@localhost>
2013-10-13 13:19:23 -0500

    + Template inicial + Inclusión JS y plugins

--- index.php
+++ index.php
@@ -1,17 +1,51 @@
 <?php
-
 /*
  * Sneyder Navia
  * fabiansneyder@gmail.com
  * Copyright 2013
  */
-
 ?>
 <html>
     <head>
-        <title>X</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Proyecto Estad&iacute;stica I</title>
+        <link href="assets/css/styles.css" rel="stylesheet" media="screen">
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     </head>
     <body>
-        
+        <div id="main_wrap">
+            <div class="navbar navbar-inverse navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">I.T.C</a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Inicio</a></li>
+                            <li><a href="javascript:">Acerca de...</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="page-header main_title">
+                    <h1>Indicadores de Tendencia Central &amp; Gr&aacute;ficas</h1>
+                </div>
+                
+            </div>
+        </div>
+        <div id="main_footer">
+            <div class="container">
+                <p class="credit">Sneyder Navia Urbano <span>&COPY;</span> 2013</p>
+            </div>
+        </div>
+        <!-- INCLUDE JS FILES -->
+        <script src="assets/libraries/jquery.10.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     </body>
 </html>
